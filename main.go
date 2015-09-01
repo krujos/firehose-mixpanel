f84b75cde0c6e23657adb85703c0fea5e2d7b544
krujos <jkruck@pivotal.io>
2015-08-31 21:50:28 -0500

    Get a token from uaa

--- main.go
+++ main.go
@@ -4,9 +4,12 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"net/url"
+	"os"
 	"strconv"
 
 	"github.com/cloudfoundry-community/cfenv"
+	"github.com/krujos/uaaclientcredentials"
 )
 
 func root(w http.ResponseWriter, req *http.Request) {
@@ -30,7 +33,34 @@ func getToken(appEnv cfenv.Services) string {
 func main() {
 	appEnv, _ := cfenv.Current()
 	setupHTTP(appEnv.Port)
-	token := getToken(appEnv.Services)
+	uaa, err := GetUserProvidedServiceByName("uaa", appEnv)
+	if nil != err {
+		log.Fatal("Could not find uaa service, did you bind it?")
+		os.Exit(1)
+	}
 
-	log.Printf("Using token" + token)
+	uaaURL, err := url.Parse(uaa.Credentials["uri"].(string))
+	if nil != err {
+		log.Fatal("Could not parse uaa URI")
+		log.Fatal(err)
+		os.Exit(1)
+	}
+
+	uaaConnection, err := uaaclientcredentials.New(uaaURL, true, uaa.Credentials["client_id"].(string),
+		uaa.Credentials["client_secret"].(string))
+
+	if nil != err {
+		log.Fatal("Failed to setup uaa connection")
+		log.Fatal(err)
+		os.Exit(1)
+	}
+
+	token, err := uaaConnection.GetBearerToken()
+
+	if nil != err {
+		log.Fatal("Failed to get token from UAA")
+		log.Fatal(err)
+		os.Exit(1)
+	}
+
 }
